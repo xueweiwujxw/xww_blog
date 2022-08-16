b69be3b5323adad7f282981472fa843c7e50fda6
xueweiwujxw <xueweiwujxw@outlook.com>
2022-08-16 22:47:47 +0800

    🐳 chore(deploy): 修改发布脚本流程

--- deploy.sh
+++ deploy.sh
@@ -3,25 +3,12 @@
 # never timeout
 set timeout -1
 
-spawn rm -rf public
-spawn hugo
-expect eof
-
 # delete outdated pack
-spawn ssh ubuntu@wlanxww.com
+spawn ssh ubuntu@43.154.179.178
 expect "*" {
-    send "rm -rf ~/public \r"
-    send "exit \r"
-}
-expect eof
-
-# upload upgraded pack
-spawn scp -r ./public ubuntu@wlanxww.com:~/
-expect eof
-
-# sync to nginx
-spawn ssh ubuntu@wlanxww.com
-expect "*" {
+    send "test -d ~/xww_blog && { cd ~/xww_blog; git pull --recurse-submodules; git submodule update --remote; } || { git clone https://github.com/xueweiwujxw/xww_blog.git --recurse-submodules; }\r"
+    send "cd ~/xww_blog \r"
+    send "hugo --minify --gc \r"
     send "sudo rm -rf /var/www/html/* \r"
     send "sudo cp -rvf ~/public/* /var/www/html/ \r"
     send "exit \r"
