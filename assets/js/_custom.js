b2c96e7edc34d1b43966cc0e3241c74aceae3d60
xueweiwujxw <xueweiwujxw@outlook.com>
2022-09-04 12:32:57 +0800

    ✨ feat(aside): 添加aside以及样式

---
+++ assets/js/_custom.js
@@ -0,0 +1,4 @@
+const $sakana = document.getElementById('aside-sakana');
+$sakana.style.visibility = 'visible';
+$sakana.style.marginTop = document.querySelector('.single-title').clientHeight + document.querySelector('.post-meta').clientHeight + 'px';
+$sakana.style.marginBottom = document.getElementById('post-footer').clientHeight + 'px';
