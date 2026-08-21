1228815f4aec75797086dee1ac6af72e72d92084
wlanxww <xueweiwujxw@outlook.com>
2026-08-21 15:24:07 +0800

    fix: migrate blog to FixIt v1
    * fix: migrate blog to FixIt v1
    
    * fix: migrate blog to FixIt v1
    
    * fix: migrate blog to FixIt v1
    
    * fix: migrate blog to FixIt v1
    
    * fix: migrate blog to FixIt v1
    
    * fix: migrate custom assets to FixIt v1
    
    * fix: migrate custom assets to FixIt v1
    
    * fix: migrate custom assets to FixIt v1
    
    * fix: migrate custom assets to FixIt v1

--- assets/js/_custom.js
+++
@@ -1,4 +0,0 @@
-// const $sakana = document.getElementById('sakana-item');
-// $sakana.style.visibility = 'visible';
-// $sakana.style.marginTop = document.querySelector('.single-title').clientHeight + document.querySelector('.post-meta').clientHeight + 'px';
-// $sakana.style.marginBottom = document.getElementById('post-footer').clientHeight + 'px';
